9c922dc1f319ea32bfa46b457dd54976f652a93a
A. Stoewer <adrian.stoewer@rz.ifi.lmu.de>
2013-06-19 18:06:14 +0200

    Renamed modules: virtfs --> morphfs virtfile --> fshelper Module fshelper almost complete

---
+++ morphdepot/morphfs.py
@@ -0,0 +1,14 @@
+# Base classes for morphfs
+
+from __future__ import division, unicode_literals, print_function
+from morphdepot.defaultfs import DefaultFS
+
+
+class MorphFS(DefaultFS):
+    """
+    Main fuse interface of the morphdepot file system.
+    """
+    pass
+
+
+
