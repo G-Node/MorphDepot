709edabcd67e8007f250d8ef90a401e173dd2c5d
A. Stoewer <adrian.stoewer@rz.ifi.lmu.de>
2013-06-28 12:49:23 +0200

    Logging updated

--- morphdepot/log.py
+++ morphdepot/log.py
@@ -1,23 +1,33 @@
+from __future__ import division, unicode_literals, print_function
+
 import logging
 
 LEVEL = logging.DEBUG
 LOGFILE = "morph.log"
 
-logging.basicConfig(level=LEVEL, filename=LOGFILE)
+logging.basicConfig(level=LEVEL)
 
 
-def logged(func, level=logging.INFO):
+def logged(func):
 
-    def wrap(*args, **kwargs):
+    def make_fqn(func, args):
         fqn = ""
+
         if func.__module__ is not None:
             fqn += func.__module__ + "."
         if func.__class__ is not None:
             fqn += func.__class__.__name__ + "."
-        fqn += func.__name__ + "( " + repr(*args) + " )"
-        logging.log(level, "going go call %s", fqn)
+        fqn += func.__name__ + "(" + repr(args) + ")"
+
+        return fqn
+
+    def wrap(*args, **kwargs):
+        fqn = make_fqn(func, **kwargs)
+        logging.log(LEVEL, "going go call %s", fqn)
+
         result = func(*args, **kwargs)
-        logging.log(level, "result of %s was %s", fqn, str(result))
+
+        logging.log(LEVEL, "result of %s was %s", fqn, str(result))
         return result
 
     return wrap
