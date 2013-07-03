e8a70d6af7ddc68bea5116ea7f73c7e0c559a025
A. Stoewer <adrian.stoewer@rz.ifi.lmu.de>
2013-07-03 11:03:02 +0200

    init_db.py: check for search path caused problems when using default schema and postgres

--- morphdepot/config.py
+++ morphdepot/config.py
@@ -1,21 +1,22 @@
-# DB = {
-#     'url': 'postgresql://dev_rautenbe@ama-prod/develop',
-#     'type': 'postgresql',
-#     'echo': False,
-#     'schema': 'ginjang_ndb',
-#     'add_test_data': True,
-#     'pg_recreate_schema': True
-# }
-
 DB = {
-    'url': 'sqlite://',
-    'type': 'sqlite',
-    'echo': False,
-    'schema': 'ginjang_ndb',
+    'url': 'postgresql://test:test@localhost/test',
+    'type': 'postgresql',
+    'echo': True,
+    'schema': 'public',
     'add_test_data': True,
+    'pg_recreate_schema': False
 }
+
+#DB = {
+#    'url': 'sqlite://',
+#    'type': 'sqlite',
+#    'echo': False,
+#    'schema': 'ginjang_ndb',
+#    'add_test_data': True,
+#}
 
 RAW_DATA = {
     'root_dir': '/tmp/MorphDepot/raw_data',
-    'tmp_dir': '/tmp'}
+    'tmp_dir': '/tmp'
+}
 
