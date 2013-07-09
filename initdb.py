38d6d88f07574bb5d58d27b3be02b1de7e4e27b8
A. Stoewer <adrian.stoewer@rz.ifi.lmu.de>
2013-07-09 15:49:33 +0200

    initdb: add more things to the database

--- initdb.py
+++ initdb.py
@@ -59,6 +59,7 @@ def db_create():
 @logged
 def db_populate():
     if connected:
+
         # Add Scientist
         # #############
         scientist = Scientist(
@@ -68,9 +69,26 @@ def db_populate():
             author_notation="Philipp L. Rautenberg",
             affiliations="Ludwig-Maximilians-Universitaet Muenchen"
         )
+        session.add(scientist)
 
+        scientist = Scientist(
+            first_name="Andrey",
+            last_name="Sobolev",
+            author_notation="A. Sobolev",
+            affiliations="Ludwig-Maximilians-Universitaet Muenchen"
+        )
+        session.add(scientist)
+
+        scientist = Scientist(
+            first_name="Adrian",
+            last_name="Stoewer",
+            author_notation="Adrian D. Stoewer",
+            affiliations="Ludwig-Maximilians-Universitaet Muenchen"
+        )
         session.add(scientist)
         session.commit()
+
+        all_scientists = session.query(Scientist).order_by(Scientist.author_notation).all()
 
         # Add Dimensions and Animal
         # #########################
@@ -99,57 +117,112 @@ def db_populate():
                 session.add(data)
         session.commit()
 
-        animal = Animal(
-            label="Testtier",
-            age=6,
-            species="apis mellifera",
-            company="Nonogaki",
-            colony='colony_1',
-            labor_state='nurse')
+        animal = Animal(label="Testtier", age=6, species="apis mellifera", company="Nonogaki",
+                        colony='colony_1', labor_state='nurse')
         session.add(animal)
         session.commit()
-
 
         # Add Expermiment
         # ###############
         experiment = Experiment(
-            label="my first experiment",
+            label="Exp 01",
             date=datetime.datetime.now(),
             lab_notebook="here is my labbook entry"
         )
-        scientist.experiments.append(experiment)
+        all_scientists[2].experiments.append(experiment)
+
+        experiment = Experiment(
+            label="Exp 02",
+            date=datetime.datetime.now(),
+            lab_notebook="here is my labbook entry"
+        )
+        all_scientists[2].experiments.append(experiment)
+
+        experiment = Experiment(
+            label="Exp 03",
+            date=datetime.datetime.now(),
+            lab_notebook="here is my labbook entry"
+        )
+        all_scientists[1].experiments.append(experiment)
+
+        experiment = Experiment(
+            label="Exp 04",
+            date=datetime.datetime.now(),
+            lab_notebook="here is my labbook entry"
+        )
+        all_scientists[0].experiments.append(experiment)
         session.commit()
 
-        # Add TissueSample
-        ##################
-        tissue = TissueSample(label="test-tissue")
-        tissue.animal = animal
-        tissue.experiment = experiment
-        session.commit()
+        all_experiments = session.query(Experiment).order_by(Experiment.label).all()
 
         # Add Protocol
         ##############
-        protocol = Protocol(label="protocol 1", description="did something")
-        tissue.protocols.append(protocol)
+        session.add(Protocol(label="Protocol 01", description="did something"))
+        session.add(Protocol(label="Protocol 02", description="did something really cool"))
+        session.add(Protocol(label="Protocol 03", description="apply liquid a to unknown thing"))
+        session.add(Protocol(label="Protocol 04", description="this protocol can't work at all"))
+        session.add(Protocol(label="Protocol 05", description="did something else"))
+        session.add(Protocol(label="Protocol 06", description="apply background dye"))
+        session.commit()
+
+        all_protocols = session.query(Protocol).order_by(Protocol.label).all()
+
+        # Add TissueSample
+        ##################
+        tissue = TissueSample(label="Tissue 01")
+        tissue.animal = Animal(label="Bee 01", age=6, species="apis mellifera", company="Nonogaki",
+                               colony="colony_1", labor_state="foreigner")
+        tissue.experiment = all_experiments[0]
+        tissue.protocols.append(all_protocols[0])
+        tissue.protocols.append(all_protocols[2])
+
+        tissue = TissueSample(label="Tissue 02")
+        tissue.animal = Animal(label="Bee 02", age=6, species="apis mellifera", company="Akiyta-ya",
+                               colony="colony_2", labor_state="nurse")
+        tissue.experiment = all_experiments[0]
+        tissue.protocols.append(all_protocols[4])
+        tissue.protocols.append(all_protocols[5])
+
+        tissue = TissueSample(label="Tissue 03")
+        tissue.animal = Animal(label="Bee 04", age=6, species="apis cerena", company="Nonogaki",
+                               colony="colony_1", labor_state="nurse")
+        tissue.experiment = all_experiments[1]
+        tissue.protocols.append(all_protocols[3])
+
+        tissue = TissueSample(label="Tissue 04")
+        tissue.animal = Animal(label="Bee 06", age=6, species="apis mellifera", company="Akiyta-ya",
+                               colony="colony_2", labor_state="foreigner")
+        tissue.experiment = all_experiments[1]
+        tissue.protocols.append(all_protocols[0])
+        tissue.protocols.append(all_protocols[1])
+        tissue.protocols.append(all_protocols[4])
+        session.commit()
+
+        all_tissues = session.query(TissueSample).order_by(TissueSample.label).all()
 
         # Add Neuron
         ############
-        neuron = Neuron(label="my favorite neuron!")
+        session.add(Neuron(label="Neuron 01"))
+        session.add(Neuron(label="Neuron 02"))
+        session.add(Neuron(label="Neuron 03"))
+        session.add(Neuron(label="Neuron 04"))
+        session.commit()
+
+        all_neurons = session.query(Neuron).order_by(Neuron.label).all()
 
         # Add NeuroRepresentation
         ################################
-        nr = NeuroRepresentation(label='first file associated with a neuron')
-        tissue.neuro_representations.append(nr)
-        # nr.tissue_sample = tissue
-        session.commit()
-        nr.neurons.append(neuron)
-        session.commit()
+        #nr = NeuroRepresentation(label='first file associated with a neuron')
+        #nr.tissue_sample = all_tissues[0]
+        #nr.neurons.append(all_neurons[0])
+
+        #session.commit()
 
         # File
         ######
-        file_object = nr.add_file("initdb.py")
-        print(file_object.st_size)
-        session.commit()
+        #file_object = nr.add_file("initdb.py")
+        #print(file_object.st_size)
+        #session.commit()
 
         # Permission
         animal.permission = Permisson()
@@ -159,45 +232,90 @@ def db_populate():
         #########
         # morph #
         #########
-        mi = MicroscopeImage(label="my first microscope image")
-        mis = MicroscopeImageStack(label="my first microscope image stack")
+        mi = MicroscopeImage(label="Image 01")
+        mi.add_file("initdb.py")
+        mi.neurons.append(all_neurons[0])
+        mi.tissue_sample = all_tissues[0]
 
-        tissue.neuro_representations.append(mi)
-        tissue.neuro_representations.append(mis)
+        mi = MicroscopeImage(label="Image 02")
+        mi.add_file("initdb.py")
+        mi.neurons.append(all_neurons[0])
+        mi.tissue_sample = all_tissues[0]
+
+        mi = MicroscopeImage(label="Image 03")
+        mi.add_file("initdb.py")
+        mi.neurons.append(all_neurons[1])
+        mi.tissue_sample = all_tissues[1]
+
+        mi = MicroscopeImage(label="Image 04")
+        mi.add_file("initdb.py")
+        mi.tissue_sample = all_tissues[1]
+
+        mi = MicroscopeImage(label="Image 05")
+        mi.add_file("initdb.py")
+        mi.neurons.append(all_neurons[2])
+        mi.tissue_sample = all_tissues[2]
         session.commit()
 
-        seg = Segmentation(label="my first segmentation")
-        seg.scientist = scientist
-        seg.microscope_image_stack = mis
-        seg.tissue_sample = tissue
+        all_images = session.query(MicroscopeImage).order_by(MicroscopeImage.label).all()
+
+        mis = MicroscopeImageStack(label="Image stack 01")
+        mis.add_file("initdb.py")
+        mis.neurons.append(all_neurons[0])
+        mis.tissue_sample = all_tissues[0]
+
+        mis = MicroscopeImageStack(label="Image stack 02")
+        mis.add_file("initdb.py")
+        mis.neurons.append(all_neurons[0])
+        mis.tissue_sample = all_tissues[1]
+
+        mis = MicroscopeImageStack(label="Image stack 03")
+        mis.add_file("initdb.py")
+        mis.tissue_sample = all_tissues[1]
+
+        mis = MicroscopeImageStack(label="Image stack 04")
+        mis.add_file("initdb.py")
+        mis.tissue_sample = all_tissues[2]
+
+        mis = MicroscopeImageStack(label="Image stack 05")
+        mis.add_file("initdb.py")
+        mis.tissue_sample = all_tissues[2]
         session.commit()
+
+        all_image_stacks = session.query(MicroscopeImageStack).order_by(MicroscopeImageStack.label).all()
+
+        #seg = Segmentation(label="my first segmentation")
+        #seg.scientist = scientist
+        #seg.microscope_image_stack = mis
+        #seg.tissue_sample = tissue
+        #session.commit()
 
         # working with dimensions
-        session.add(Software(name="SIGEN"))
-        session.commit()
-        param = GeneralParam(name='Param1')
-        seg.software = "SIGEN"
-        seg.general_params.append(param)
-        session.commit()
+        #session.add(Software(name="SIGEN"))
+        #session.commit()
+        #param = GeneralParam(name='Param1')
+        #seg.software = "SIGEN"
+        #seg.general_params.append(param)
+        #session.commit()
 
         #########
         # ephys #
         #########
-        session.add(SpontaneousActivity(name="first spontaneous activity"))
-        response_property = ResponseProperty(name="first response Property")
-        session.add(response_property)
-        session.commit()
+        #session.add(SpontaneousActivity(name="first spontaneous activity"))
+        #response_property = ResponseProperty(name="first response Property")
+        #session.add(response_property)
+        #session.commit()
 
-        ephys = Electrophysiology(
-            label="my first electrophysiology",
-            spontaneous_activity='first spontaneous activity',
-            quality_rank=1,
-            stim_vibration_frequency=1.5,
-            tissue_sample=tissue,
-            response_properties=[response_property]
-        )
+        #ephys = Electrophysiology(
+        #    label="my first electrophysiology",
+        #    spontaneous_activity='first spontaneous activity',
+        #    quality_rank=1,
+        #    stim_vibration_frequency=1.5,
+        #    tissue_sample=tissue,
+        #    response_properties=[response_property]
+        #)
         # tissue.neuro_representations.append(ephys)
-        session.commit()
+        #session.commit()
 
 # Do things ...
 if __name__ == "__main__":
